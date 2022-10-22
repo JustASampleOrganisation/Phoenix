814d3b0b6f647fca14a076d5684adf9392a7ee34
= <=>
2022-10-22 17:28:32 +0300

    scroll?

---
+++ phoenix/static/js/index.js
@@ -0,0 +1,12 @@
+$('.start-menu-handler').click(function (e) { 
+    $.ajax({
+        type: "GET",
+        url: "/get_app_frame?frame=4",
+        success: function (response) {
+            document.querySelector('body').innerHTML = response;
+        },
+        failed: function (error) {
+            alert(error);
+        }
+    });
+});
