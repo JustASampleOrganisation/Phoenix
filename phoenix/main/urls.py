417cd12a7b46271254c2d2fa01263304929e9652
SuperTapok <noname20041@mail.ru>
2022-10-22 21:22:15 +0700

    Доработки

--- phoenix/main/urls.py
+++ phoenix/main/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'), 
     path('login/', LoginUserView.as_view(), name='login'), 
     path('logout/', logout_user, name='logout'),
+    path('get_basket', get_basket, name='get_basket'),    
+    path('add_to_basket', add_to_basket, name='add_to_basket'),
 ]
